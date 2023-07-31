5a5e1b164ef2905771b2fd4d40850f46ee7a1c39
Lollytim <ololadetimileyin3@gmail.com>
2023-07-31 21:10:15 +0100

    updated all

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Sectionhead from './Component/Section1/Sectionhead';
 import Section2 from './Component/Section2/Section-2';
 import Section3 from './Component/Section3/Section-3';
+import Section4 from './Component/Section4/Section-4';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Sectionhead />
       <Section2 />
       <Section3 />
+      <Section4 />
     </div>
   );
 }
