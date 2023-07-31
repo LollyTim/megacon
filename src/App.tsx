2478b440b03039aafc619fb420fc74bcb979ccbf
Lollytim <ololadetimileyin3@gmail.com>
2023-07-31 17:53:19 +0100

    updated section3

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Sectionhead from './Component/Section1/Sectionhead';
 import Section2 from './Component/Section2/Section-2';
+import Section3 from './Component/Section3/Section-3';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Sectionhead />
       <Section2 />
+      <Section3 />
     </div>
   );
 }
---
+++ src/Component/Section3/Section-3.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import './Section-3.css'
+
+const Section3 = () => {
+    return (
+        <section className='section3'>
+            <div className='container4all'>
+
+                <div className='container1'>
+                    <div className='container4dcontent'>
+                        <div className="content1">
+                            <h3 className='first-text3'>Find a co-founder in our network</h3>
+                            <p className='firstp3'>There’s nothing like a journey this long being shared and carried together by people who sees your vision and run with it as close as you do</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='container1'>
+                    <div className='container4dcontent2'>
+                        <div className="content1">
+                            <h3 className='first-text3-others'>All you need from idea to launch</h3>
+                            <p className='firstp3'>Resources to build can be sometimes challenging, that is why we have put together a giant folder of resources available to build at your disposal</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='container1'>
+                    <div className='container4dcontent2'>
+                        <div className="content1">
+                            <h3 className='first-text3-others'>Launch and re-launch to target market</h3>
+                            <p className='firstp3'>Are you ready to showcase your solution to the world, launch on Megacorn and instantly reach over 10 million people</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='container1'>
+                    <div className='container4dcontent2'>
+                        <div className="content1">
+                            <h3 className='first-text3-others'>Getting first set of customer on Megacorn</h3>
+                            <p className='firstp3'>Our community has over 2 million active teams and individuals looking for products that are building</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='container1'>
+                    <div className='container4dcontent2'>
+                        <div className="content1">
+                            <h3 className='first-text3-others'>Need to raise? No problem!</h3>
+                            <p className='firstp3'>Connect with investors that are looking for wonderful products and team like yours to back for the future</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='containerRight'>
+                <img src="Step.png" alt="" />
+            </div>
+
+        </section>
+    )
+}
+
+export default Section3
