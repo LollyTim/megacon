892a8aabf95275633d72907b55bce39c443d7adf
Lollytim <ololadetimileyin3@gmail.com>
2023-07-31 14:11:45 +0100

    changed some text

--- src/Component/Section2/Section-2.tsx
+++ src/Component/Section2/Section-2.tsx
@@ -11,14 +11,14 @@ const Section2 = () => {
                 <h3>All you need from idea to launch</h3>
                 <p>From collating your ideas to finding co-founders & founding teams to first launch and finding early adopters</p>
                 <div className='cardcontainer'>
-                    <Card title='ideation' text='leorem ispieum dommy twxt text' image={<img src='pen.png' alt='' />} />
-                    <Card title='ideation' text='leorem ispieum dommy twxt text' image={<img src='handshake.png' alt='' />} />
-                    <Card title='ideation' text='leorem ispieum dommy twxt text' image={<img src='code.png' alt='' />} />
+                    <Card title='Ideation' text='leorem ispieum dommy twxt text' image={<img src='pen.png' alt='' />} />
+                    <Card title='Co-founder' text='leorem ispieum dommy twxt text' image={<img src='handshake.png' alt='' />} />
+                    <Card title='Building' text='leorem ispieum dommy twxt text' image={<img src='code.png' alt='' />} />
                 </div>
                 <div className='cardcontainer'>
-                    <Card title='ideation' text='leorem ispieum dommy twxt text' image={<img src='rocket.png' alt='' />} />
-                    <Card title='ideation' text='leorem ispieum dommy twxt text' image={<img src='adminlinks.png' alt='' />} />
-                    <Card title='ideation' text='leorem ispieum dommy twxt text' image={<img src='moneybag.png' alt='' />} />
+                    <Card title='Lunch' text='leorem ispieum dommy twxt text' image={<img src='rocket.png' alt='' />} />
+                    <Card title='Distribution' text='leorem ispieum dommy twxt text' image={<img src='adminlinks.png' alt='' />} />
+                    <Card title='Investment' text='leorem ispieum dommy twxt text' image={<img src='moneybag.png' alt='' />} />
                 </div>
             </section>
         </div>
