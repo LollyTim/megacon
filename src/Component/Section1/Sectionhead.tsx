1c68e12eb2495da007316711c04abb5bba6c5768
Lollytim <ololadetimileyin3@gmail.com>
2023-08-07 18:34:14 +0100

    updated dark mode

--- src/Component/Section1/Sectionhead.tsx
+++ src/Component/Section1/Sectionhead.tsx
@@ -10,14 +10,11 @@ const Sectionhead = () => {
                 backgroundRepeat: 'no-repeat',
                 backgroundSize: "650px",
                 backgroundPositionY: "70px",
-                backgroundColor: `var(--primary-color)`,
-                fill: "black"
-
-
+                backgroundColor: `var(--primary-color)`
             }} >
                 <div className='section1'>
                     <div className='Star1'><img src="Star.png" alt="" /></div>
-                    <div className='text1'><h1 className='line1'>Find and launch <div className='colored1'>digital</div></h1> <h1 className='line2'><br /> <div className='colored'>products </div> for Aftica</h1></div>
+                    <div className='text1'><h1 className='line1'>Find and launch <div className='colored1'>digital</div></h1> <h1 className='line2'><br /> <div className='colored'>products </div> for Africa</h1></div>
                     <div className='text2'> We connect creators & teams to their desired market of customers, partners. <br /> talents and investors needed to build great companies</div>
                     <div className='btns2'>
                         <div className='Polygon'><img src="Polygon.png" alt="" /></div>
